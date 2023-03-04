099d3387f2b2640a4041bcc1c9fc48584c7a7e6d
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-03-03 22:02:11 -0500

    added spinner

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 import "./org-vignette.js";
 
 const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const displayQuery = document.querySelector("query");
-displayQuery.innerHTML = urlParams.get("org")[0].toUpperCase() + urlParams.get("org").substring(1) + "...";
+const urlParams = new URLSearchParams(queryString) ;
+
+
+if (urlParams.get("org")) {
+  searchOrgs(urlParams.get("org"));
+}
+
 
 async function searchOrgs(query) {
+  document.getElementById("buttonSpinner").classList.remove("visually-hidden")
+  document.getElementById("buttonReady").classList.add("visually-hidden")
+
   const main = document.querySelector("main");
   const api = "https://api.artsdata.ca/recon";
   const payload = { query: query, type: "schema:Organization" };
   const urlParams = new URLSearchParams(payload);
   const url = `${api}?${urlParams}`;
+
   const res = await fetch(url);
   const json = await res.json();
-  console.log(json);
 
+  console.log(json);
+  const displayQuery = document.querySelector("query");
+  displayQuery.innerHTML = "Results for " + query[0].toUpperCase() + query.substring(1) + "...";
   json.result.forEach((org) => {
-    console.log(org.name)
     const el = document.createElement("org-vignette");
     el.org = org;
     main.appendChild(el);
   });
 
+  document.getElementById("buttonSpinner").classList.add("visually-hidden")
+  document.getElementById("buttonReady").classList.remove("visually-hidden")
+
 }
-searchOrgs(urlParams.get("org"));
+
