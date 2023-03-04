c8721cf8bc7237001a45b21e3b4bda8fd06f9166
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-03-03 23:33:35 -0500

    added webpage url

--- org-vignette.js
+++ org-vignette.js
@@ -3,10 +3,10 @@ class OrgVignette extends HTMLElement {
  
     this.innerHTML = `
       <div  class="list-group">
-      <a ${org.id[0] != "K" ? "" : "href='http://kg.artsdata.ca/resource/" + encodeURIComponent(org.id) + "'"} class="${org.match && "list-group-item-primary" } list-group-item list-group-item-action d-flex justify-content-between align-items-start">
+      <a ${org.id[0] != "K" ? "" : "href='http://kg.artsdata.ca/resource/" + encodeURIComponent(org.id) + "'"} class="${org.match && org.id[0] == "K" && "list-group-item-primary" } list-group-item list-group-item-action d-flex justify-content-between align-items-start">
       <div class="ms-2 me-auto">
       <div class="fw-bold">${encodeHTMLEntities(truncate(org.name))}</div>
-      ${org.type[0].name} ${org.description && " - "} ${encodeHTMLEntities(org.description)}
+      ${org.type[0].name} ${org.description && " - " + encodeHTMLEntities(org.description)}  ${org.type[0].name != "WebPage" ? "" :  " - " + org.id.substr(4, 100)}
       </div>
       <span class="badge bg-secondary rounded-pill">${org.id[0] == "K" ? org.id : ""}</span>
       </a>
